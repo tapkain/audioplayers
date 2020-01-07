7a4d6c7b428ebc6738d86cc809599caf6ed18dd5
Yevhenii Velizhenkov <11300345+tapkain@users.noreply.github.com>
2020-01-07 13:53:33 +0100

    Update AudioplayersPlugin.m

--- ios/Classes/AudioplayersPlugin.m
+++ ios/Classes/AudioplayersPlugin.m
@@ -412,9 +412,9 @@ float _playbackRate = 1.0;
 
   // code moved from play() to setUrl() to fix the bug of audio not playing in ios background
   NSError *error = nil;
-  AVAudioSessionCategory category = respectSilence ? AVAudioSessionCategoryAmbient : AVAudioSessionCategoryOptionDuckOthers;
+  AVAudioSessionCategory category = respectSilence ? AVAudioSessionCategoryAmbient : AVAudioSessionCategoryPlayback;
     
-  BOOL success = [[AVAudioSession sharedInstance] setCategory:category withOptions:AVAudioSessionCategoryOptionMixWithOthers error:&error];
+  BOOL success = [[AVAudioSession sharedInstance] setCategory:category withOptions:AVAudioSessionCategoryOptionDuckOthers error:&error];
     
   if (!success) {
     NSLog(@"Error setting speaker: %@", error);
